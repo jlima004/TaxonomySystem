b2316d863131d904802cdd5ce2588db148f640d2
Jefferson Lima <jlima004@gmail.com>
2026-05-21 14:12:02 -0300

    feat(06-02): export compiler public APIs
    - Add barrel exports for artifact compilers and writer
    - Expose public compile option and result types

--- src/compiler/index.ts
+++ src/compiler/index.ts
@@ -17,3 +17,20 @@ export {
   validateSimilarityGraph,
   validateAllOutputs,
 } from './validate_output.js'
+
+export { compileTaxonomy } from './compile_taxonomy.js'
+export type { CompileTaxonomyOptions } from './compile_taxonomy.js'
+
+export { compileAliases } from './compile_aliases.js'
+export type { CompileAliasesOptions } from './compile_aliases.js'
+
+export { compileAll } from './compile_all.js'
+export type { CompileAllInputs, CompileAllOptions, CompileAllResult } from './compile_all.js'
+
+export {
+  ALIASES_FILENAME,
+  CompileWriteError,
+  SIMILARITY_FILENAME,
+  TAXONOMY_FILENAME,
+  writeCompileResults,
+} from './write_outputs.js'
